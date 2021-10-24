30ebaeb2acf5f5991215986f584dd38e04ac424c
E1Hai <eyashaidar@gmail.com>
2021-10-24 20:50:22 +0200

    Das erste Commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eyas/catkin_ws_kulun/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/eyas/catkin_ws_kulun/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/eyas/catkin_ws_kulun/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/eyas/catkin_ws_kulun/src:$ROS_PACKAGE_PATH"
